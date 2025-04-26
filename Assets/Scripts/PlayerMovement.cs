039ee3a9f77ef852c08affafc931e4ebfe0f2b48
GaelGalvan <112572380+GaelGalvan@users.noreply.github.com>
2025-04-26 06:21:05 -0500

    Added Objects and click

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,16 +10,41 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
 
 
+    public Transform player;
+    [Header("Character Layers")] 
+    public LayerMask Matador;
+    public LayerMask Woman;
+    public LayerMask Napoleon;
+
+
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
-
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime); 
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        // Left Click
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.CheckSphere(player.position, 5f, Matador))
+            { // Write code here for matador
+                Debug.Log("NEAR MATADOR AND CLICKED");
+            }
+
+            if(Physics.CheckSphere(player.position, 5f, Woman))
+            { // Write code here for Rebecca?
+                Debug.Log("NEAR WOMAN AND CLICKED");
+            }
+            
+            if(Physics.CheckSphere(player.position, 5f, Napoleon))
+            { // Write code here for Napoleon
+                Debug.Log("NEAR Napoleon AND CLICKED");
+            }
+        } 
 
         
     }
